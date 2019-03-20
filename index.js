5ac63c51e60dfc035d0cd7de1efbea1d14abc630
Saint-Jean Germila <germila@noukod.com>
2019-03-20 19:13:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,25 +14,25 @@ class Neighborhood{
      store.neighborhoods.push(this);
   }
 }
-// 
+//
 // deliveries(){
 //   return store.deliveries.filter(delivery => delivery.neighborhoodId === this.id);
 // }
 // customers(){
 //   return
 // }
-// 
+//
 // class Neighborhood{
 //   constructor(name){
 //     this.id = ++neighborhoodId;
 //     this.name = name;
-// 
+//
 //      store.neighborhoods.push(this)
 //   }
 //   class Neighborhood{
 //     constructor(name){
 //       this.id = ++neighborhoodId;
 //       this.name = name;
-// 
+//
 //        store.neighborhoods.push(this)
 //     }
