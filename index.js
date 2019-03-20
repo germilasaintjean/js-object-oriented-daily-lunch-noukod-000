fdce4edfc92c931a8789960e250ffad6612dae40
Saint-Jean Germila <germila@noukod.com>
2019-03-20 18:48:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ let neighborhoodId = 0;
 let customerId = 0;
 let mealId = 0;
 let deliveryId = 0;
+
+class Neighborhood{
+  constructor(name){
+    this.id = ++neighborhoodId;
+    this.name = name;
+
+     store.neighborhoods.push(this)
+  }
+}
+
+deliveries(){
+  return store.deliveries.filter(delivery => delivery.neighborhoodId === this.id);
+}
